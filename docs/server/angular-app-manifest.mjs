3f21ca886f7b45a0ceba734a357defc743b3da22
ricsie_cruz <ricsie_aguila@yahoo.com>
2024-12-22 14:58:11 +0800

    deploy

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -13,7 +13,7 @@ export default {
   assets: {
     'index.csr.html': {size: 4959, hash: 'ebc6e26906d424ba1aff111468396788e2798ca73f38c5225befb52b13422edb', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
     'index.server.html': {size: 1058, hash: 'b9e67de26227e23c6aa09ca0c21e8efb1855d5686f0281ab9792d5b1b2c19da7', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 14245, hash: '8278ffebdcbf40642c46c76cadf94146462fd0c01c2f47425e5c7deb9fe6fb9f', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.html': {size: 14704, hash: '80cba996dcd5ae6163eaf1164d574143e5b442464c31e92fe872cf0abe9f0a5a', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-LHVB35F3.css': {size: 231642, hash: 'ZrexhCQiv/A', text: () => import('./assets-chunks/styles-LHVB35F3_css.mjs').then(m => m.default)}
   },
 };
